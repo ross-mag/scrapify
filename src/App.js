ad0c69406ca15e2c3dd793f14fc0b85eeb9f2cca
Ross Magparangalan <rossmagp@gmail.com>
2023-06-20 12:06:58 -0400

    add nav bar and header coorelated to page routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.scss";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header/Header";
 import HomePage from "./pages/HomePage/HomePage";
 import PlaylistPage from "./pages/PlaylistPage/PlaylistPage";
 import SongsPage from "./pages/SongsPage/SongsPage";
@@ -10,12 +11,13 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Header />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/playlist" element={<PlaylistPage />} />
           <Route path="/songs" element={<SongsPage />} />
           <Route path="/notes" element={<NotesPage />} />
-          <Route path="/scrapbook-page" element={<ScrapbookPage />} />
+          <Route path="/scrapbook" element={<ScrapbookPage />} />
         </Routes>
       </BrowserRouter>
     </div>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,10 @@
 import "./Header.scss";
+import Nav from "../Nav/Nav";
 
 function Header() {
     return (
         <div className="header">
-
+            <Nav />
         </div>
     );
 }
