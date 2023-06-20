fdb9aaa7d0837ea80c2ddd232e062c6f55201925
Ross Magparangalan <rossmagp@gmail.com>
2023-06-20 11:29:05 -0400

    add page routing and directory structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import "./App.scss";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage/HomePage";
+import PlaylistPage from "./pages/PlaylistPage/PlaylistPage";
+import SongsPage from "./pages/SongsPage/SongsPage";
+import NotesPage from "./pages/NotesPage/NotesPage";
+import ScrapbookPage from "./pages/ScrapbookPage/ScrapbookPage";
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/playlist" element={<PlaylistPage />} />
+          <Route path="/songs" element={<SongsPage />} />
+          <Route path="/notes" element={<NotesPage />} />
+          <Route path="/scrapbook-page" element={<ScrapbookPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
