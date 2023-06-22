8634cb4c8e58078ff53a666ec097c5ef47730edf
Ross Magparangalan <rossmagp@gmail.com>
2023-06-22 17:03:50 -0400

    buttons and styling to the home page

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,8 +1,19 @@
 import "./HomePage.scss";
+import { Link } from "react-router-dom";
 
 function HomePage() {
   return (
     <div className="home-page">
+      <Link to={"/music"} className="button__link" activeClassName="active">
+        <button className="home__button">
+          Start adding music
+        </button>
+      </Link>
+      <Link to={"/journal"} className="button__link" activeClassName="active">
+        <button className="home__button">
+          Start journaling
+        </button>
+      </Link>
     </div>
   );
 }
