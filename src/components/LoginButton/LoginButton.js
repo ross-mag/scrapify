b173fe229840941bfdcb0083693364b59341705f
jennyxing <jennyxing27@gmail.com>
2023-06-22 17:21:17 -0400

    change login button to anchor tag

--- src/components/LoginButton/LoginButton.js
+++ src/components/LoginButton/LoginButton.js
@@ -1,20 +1,9 @@
 import "./LoginButton.scss";
-import axios from "axios";
 
 function LoginButton() {
-  const handleLogin = () => {
-    axios.get('http://localhost:1020/login')
-      .then(response => {
-        window.location.href = response.data.redirectUrl;
-      })
-      .catch(error => {
-        console.error(error);
-      });
-  };
-
   return (
     <div>
-      <button onClick={handleLogin}>Login with Spotify</button>
+      <a href="http://localhost:1020/login">Login with Spotify</a>
     </div>
   );
 }
