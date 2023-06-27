33b920566742ca1820f713331efb0d02eeba6767
Ross Magparangalan <rossmagp@gmail.com>
2023-06-26 22:10:42 -0400

    styled home page

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -5,16 +5,19 @@ function HomePage() {
   return (
     <div className="home-page">
       <h1 className="home-page__header">Scrapify</h1>
-      <Link to={"/music"} className="button__link" activeclassname="active">
-        <button className="home__button">
-          Start adding music
-        </button>
-      </Link>
-      <Link to={"/journal"} className="button__link" activeclassname="active">
-        <button className="home__button">
-          Start journaling
-        </button>
-      </Link>
+      <h3 className="home-page__subheader">Music Scrapbook</h3>
+      <div className="button__container">
+        <Link to={"/music"} className="button__link" activeclassname="active">
+          <button className="home__button">
+            Add Songs
+          </button>
+        </Link>
+        <Link to={"/journal"} className="button__link" activeclassname="active">
+          <button className="home__button">
+            Journal
+          </button>
+        </Link>
+      </div>
     </div>
   );
 }
